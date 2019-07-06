3978e537e256c28a032dd1b728a56be6ab89f81f
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-07-06 16:12:17 -0300

    métodos e atributos da class product
    métodos e atributos da class product

--- estoque/src/entities/Product.java
+++ estoque/src/entities/Product.java
@@ -1,5 +1,20 @@
 package entities;
 
 public class Product {
-
+	
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProdcts(int quantity) {
+		this.quantity -= quantity;
+	}	
 }
