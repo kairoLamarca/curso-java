29f1c058b7f736dc467bd29e3df697d0a66f3f97
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-07-06 15:17:34 -0300

    calculo dos triangulos
    calculo dos triangulos

--- triangulo/src/application/Program.java
+++ triangulo/src/application/Program.java
@@ -24,6 +24,19 @@ public class Program {
 		double p = (xA + xB + xC) / 2.0;
 		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
 		
+		p = (yA + yB + yC) / 2.0;
+		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		
+		System.out.printf("Triangule X area: %.4f%n", areaX);
+		System.out.printf("Triangule Y area: %.4f%n", areaY);
+		
+		if(areaX > areaY) {
+			System.out.println("Larger area: X");
+		}
+		else {
+			System.out.println("Larger area: Y");
+		}
+		
 		sc.close();
 
 	}
