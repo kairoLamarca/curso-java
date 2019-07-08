db9bd0f801f15c675a5ca8a0ca9d3444d0abafbc
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-07-08 16:11:22 -0300

    boxing, unboxing, wrapper
    boxing, unboxing, wrapper

---
+++ boxing_unboxing/src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int x = 20;
+
+		//Object obj = x;
+		Integer obj = x;
+
+		System.out.println(obj);
+
+		//int y = (int) obj;
+		int y = obj;
+		
+		System.out.println(y);
+
+	}
+
+}
