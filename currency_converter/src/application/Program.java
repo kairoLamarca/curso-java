698779972133f06a724a2d984b662a5fe74c9bd0
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-07-08 08:47:04 -0300

    Exercicio de conversão de dolar com classe estatica
    Exercicio de conversão de dolar com classe estatica

---
+++ currency_converter/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("What is the dollar price? ");
+		double dollarPrice = sc.nextDouble();
+		
+		System.out.print("How many dollar will be bought? ");
+		double amount = sc.nextDouble();
+		
+		System.out.printf("Amount to be paid in reais = %.2f", CurrencyConverter.dollarToReal(amount, dollarPrice));
+		
+		sc.close();
+
+	}
+
+}
