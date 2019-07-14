8b26b955a3d69de37c58f7ce85698270e5cc1dc9
Kairo Lamarca Santos <lamarca.kairo@gmail.com>
2019-07-14 17:33:04 -0300

    exemplo de polimorfismo
    exemplo de polimorfismo

--- s9_heranca/src/application/Program.java
+++ s9_heranca/src/application/Program.java
@@ -48,6 +48,13 @@ public class Program {
 		acc8.withdraw(200.0);
 		System.out.println(acc8.getBalance());
 		
+		System.out.println("-----------------------");
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
 
 	}
 
