4bb863afca9e8cec55670adf27abe2195cd7efb7
kairoLamarca <lamarca.kairo@gmail.com>
2019-07-23 12:54:44 -0300

    Stack trace para mostrar o erro que ocorreu
    Stack trace para mostrar o erro que ocorreu

--- s10_try_catch/src/application/Program.java
+++ s10_try_catch/src/application/Program.java
@@ -7,6 +7,20 @@ public class Program {
 
 	public static void main(String[] args) {
 
+		method1();
+		
+		System.out.println("End of program");
+
+	}
+	
+	public static void method1() {
+		System.out.println("**METHOD1 START**");
+		method2();
+		System.out.println("**METHOD1 END**");
+	}
+
+	public static void method2() {
+		System.out.println("**METHOD2 START**");
 		Scanner sc = new Scanner(System.in);
 
 		try {
@@ -15,12 +29,14 @@ public class Program {
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
 		} catch (InputMismatchException e) {
 			System.out.println("input error");
 		}
 
-		System.out.println("End of program");
-
 		sc.close();
+		
+		System.out.println("**METHOD2 END**");
 	}
 }
