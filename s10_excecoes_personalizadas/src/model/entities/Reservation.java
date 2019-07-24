676ab914c98c752baf8bd1f390f4a275bbda0a76
kairoLamarca <lamarca.kairo@gmail.com>
2019-07-24 13:32:17 -0300

    tratamento de exceptions com classe expecifica
    tratamento de exceptions com classe expecifica

--- s10_excecoes_personalizadas/src/model/entities/Reservation.java
+++ s10_excecoes_personalizadas/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -13,6 +15,9 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
 	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		} 
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -43,11 +48,11 @@ public class Reservation {
 		
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			throw new IllegalArgumentException("Reservation dates for update must be future dates");
+			throw new DomainException("Reservation dates for update must be future dates");
 		} 
 		
 		if (!checkOut.after(checkIn)) {
-			throw new IllegalArgumentException("Check-out date must be after check-in date");
+			throw new DomainException("Check-out date must be after check-in date");
 		} 
 		
 		this.checkIn = checkIn;
