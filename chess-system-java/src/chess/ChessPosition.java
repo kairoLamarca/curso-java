b069462ba8ed16b60b890bdf47f66ad4d67b9104
kairoLamarca <lamarca.kairo@gmail.com>
2019-07-30 13:28:10 -0300

    chessException
    chessException

--- chess-system-java/src/chess/ChessPosition.java
+++ chess-system-java/src/chess/ChessPosition.java
@@ -1,6 +1,5 @@
 package chess;
 
-import boardgame.BoardException;
 import boardgame.Position;
 
 public class ChessPosition {
@@ -10,7 +9,7 @@ public class ChessPosition {
 	
 	public ChessPosition(char column, int row) {
 		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new BoardException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
 		this.row = row;
