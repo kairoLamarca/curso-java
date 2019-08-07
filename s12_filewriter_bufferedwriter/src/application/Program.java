e51f1abcdc9d0f94ea60560862d08bd6e2a3d840
kairoLamarca <lamarca.kairo@gmail.com>
2019-08-07 13:50:23 -0300

    utilização do fileWriter e BufferedWriter
    utilização do fileWriter e BufferedWriter

---
+++ s12_filewriter_bufferedwriter/src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+
+		String path = "c:\\temp\\out.txt";
+
+		//True não reescreve
+		// try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
