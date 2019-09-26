2a5dab417960e9128591dba193df20d48452e87e
kairoLamarca <lamarca.kairo@gmail.com>
2019-09-26 12:59:49 -0300

    Buscar no banco de dados utilizando o find

--- cursojpa/src/aplicacao/Programa.java
+++ cursojpa/src/aplicacao/Programa.java
@@ -10,14 +10,21 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		/*
 		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
 		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
 		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+		*/
 		
 		//Cria conexão
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
+		/*
 		em.getTransaction().begin();
 		
 		//Salva no BD
@@ -26,7 +33,10 @@ public class Programa {
 		em.persist(p3);
 		
 		em.getTransaction().commit();
+		*/
+		
 		System.out.println("Pronto!");
+		em.close();
 		
 
 	}
