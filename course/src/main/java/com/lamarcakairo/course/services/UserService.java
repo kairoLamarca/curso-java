2f2528b1718ecff664d76fd14a28e54c4fa638df
kairoLamarca <lamarca.kairo@gmail.com>
2019-10-09 13:17:32 -0300

    Camada de serviço e registro de componentes com annotations

---
+++ course/src/main/java/com/lamarcakairo/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.lamarcakairo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lamarcakairo.course.entities.User;
+import com.lamarcakairo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
